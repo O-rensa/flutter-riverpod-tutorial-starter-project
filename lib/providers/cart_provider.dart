a899471af70f30ef6a643dec949dd0ff388beea7
Renz Lendio <131181504+O-rensa@users.noreply.github.com>
2024-08-24 18:55:56 +0800

    Add generated notifier providers

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,19 +1,15 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_files/models/product.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-class CartNotifier extends Notifier<Set<Product>> {
+part 'cart_provider.g.dart';
+
+@riverpod
+class CartNotifier extends _$CartNotifier {
 
   // initial value
   @override
   Set<Product> build() {
-    return {
-      const Product(
-        id: '4',
-        title: 'Red Backpack',
-        price: 14,
-        image: 'assets/products/backpack.png'
-      ),
-    }; 
+    return {}; 
   }
 
   // methods
@@ -29,8 +25,3 @@ class CartNotifier extends Notifier<Set<Product>> {
     }
   }
 }
-
-// provider
-final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() { 
-  return CartNotifier(); 
-});
